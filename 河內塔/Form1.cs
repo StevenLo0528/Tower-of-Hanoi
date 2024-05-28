de66387a7320bcdf8cb095e43eab06b98d87feb9
StevenLo0528 <a131835258@gmail.com>
2024-05-28 13:47:16 +0800

    .NET Framework 4.7.2

---
+++ 河內塔/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 河內塔
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(層數.Text);
+            步數.Clear();
+            hanoi(n, 'A', 'B', 'C');
+            步數.AppendText("\r\n從A移到C共走" + (Math.Pow(2, n) - 1) + "步");
+        }
+
+        void hanoi(int n, char A, char B, char C)
+        {
+            if (n == 1)
+            {
+                步數.AppendText("\r\n從" + A + "移到" + C + "\r\n");
+            }
+            else
+            {
+                hanoi(n - 1, A, C, B);
+                步數.AppendText("\r\n從" + A + "移到" + C + "\r\n");
+                hanoi(n - 1, B, A, C);
+            }
+        }
+    }
+}
